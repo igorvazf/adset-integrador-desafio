67821a8a5040cbb6ad711d9ea4830294e0536a5e
igorvazf <igorv1286@gmail.com>
2024-09-04 22:10:20 -0300

    setup inicial

---
+++ AdSetIntegrador/AdSetIntegrador.Data/Context/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AdSetIntegrador.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+    }
+}
